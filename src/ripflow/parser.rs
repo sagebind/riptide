5025c16b072554170c34c758269ca937855cdeb1
Stephen M. Coakley <me@stephencoakley.com>
2018-01-13 15:01:21 -0600

    work on reading and parsing

--- src/ripflow/parser.rs
+++ src/ripflow/parser.rs
@@ -3,52 +3,33 @@
 //! This is a handwritten, recursive descent parser. This is done both for speed
 //! and simplicity, since the language syntax is relatively simple anyway.
 //!
-//! Thanks to the scanner, the parser can actually read and parse a file of
+//! Thanks to the source, the parser can actually read and parse a file of
 //! theoretically infinite size incrementally (other than memory limits on
 //! storing the resulting AST).
-use rr::Expr;
-use scanner::*;
+use ast::Expr;
+use reader::CharReader;
 use std::borrow::Cow;
 
 
-pub struct Parser {
-    scanner: Scanner,
-    errors: Vec<ParseError>,
+/// A reference to a location in source code. Useful for error messages.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct SourcePos {
+    /// The line number. Begins at 1.
+    pub line: u32,
+
+    /// The column position in the current line. Begins at 1.
+    pub column: u32,
+
+    /// Offset fron the beginning of the source.
+    pub offset: u32,
 }
 
-impl Parser {
-    /// Create a new parser around the given scanner.
-    pub fn new(scanner: Scanner) -> Parser {
-
-    }
-}
-
-
-/// The result of a parse operation.
-#[derive(Debug)]
-pub enum ParseResult {
-    /// Parsing was successful and produced an expression.
-    Ok(Expr),
-
-    /// Parsing failed with a list of errors.
-    Fail(Vec<ParseError>),
-}
-
-impl ParseResult {
-    pub fn is_err(&self) -> bool {
-        match self {
-            &ParseResult::Ok(_) => false,
-            _ => true,
-        }
-    }
-
-    pub fn accumulate_error(mut self, error: ParseError) -> Self {
-        match self {
-            ParseResult::Ok(_) => ParseResult::Fail(vec![error]),
-            ParseResult::Fail(mut errors) => {
-                errors.push(error);
-                ParseResult::Fail(errors)
-            },
+impl Default for SourcePos {
+    fn default() -> SourcePos {
+        SourcePos {
+            line: 1,
+            column: 1,
+            offset: 0,
         }
     }
 }
@@ -65,143 +46,192 @@ pub struct ParseError {
     pub pos: SourcePos,
 }
 
-impl ParseError {
-    pub fn new<M, P>(message: M, pos: SourcePos) -> ParseError
-        where M: Into<Cow<'static, str>>
-    {
+
+pub struct Parser {
+    reader: CharReader,
+    next_char: Option<char>,
+    pos: SourcePos,
+}
+
+impl Parser {
+    /// Parse the characters from the given source into an AST.
+    pub fn parse(mut self) -> Result<Expr, ParseError> {
+        // Hey, a program is just a block!
+        self.parse_block()
+    }
+
+    fn parse_expr(&mut self) -> Result<Expr, ParseError> {
+        match self.advance() {
+
+            Some('{') => {
+                let block = self.parse_block()?;
+
+                if self.advance() == Some('}') {
+                    return Err(self.error("expected }"));
+                }
+
+                Ok(block)
+            },
+            Some('(') => self.parse_function_call(),
+            Some(')') => return Err(self.error("extra trailing parenthesis")),
+            Some('"') | Some('\'') => self.parse_string(),
+            Some(_) => self.parse_symbol(),
+            None => {
+                return Err(self.error("unexpected eof, expected expression"));
+            }
+        }
+    }
+
+    fn parse_block(&mut self) -> Result<Expr, ParseError> {
+        // A block is just a series of 0 or more expressions.
+        let mut statements = Vec::new();
+
+        loop {
+            let statement = self.parse_expr()?;
+            statements.push(statement);
+
+            if !self.match_any(&[';', '\n']) {
+                break;
+            }
+        }
+
+        Ok(Expr::Block(statements))
+    }
+
+    fn parse_function_call(&mut self) -> Result<Expr, ParseError> {
+        assert!(self.advance() == Some('('));
+        let mut items = Vec::new();
+
+        loop {
+            self.skip_whitespace();
+
+            match self.peek() {
+                Some(')') => {
+                    self.advance();
+                    break;
+                },
+                Some(_) => items.push(self.parse_expr()?),
+                None => return Err(self.error("unclosed list")),
+            }
+        }
+
+        Ok(Expr::Block(items))
+    }
+
+    fn parse_symbol(&mut self) -> Result<Expr, ParseError> {
+        let mut string = String::new();
+
+        // Read the first character of the symbol.
+        string.push(self.advance().expect("expected symbol"));
+
+        // Read any remaining characters that are part of the symbol.
+        while let Some(c) = self.peek() {
+            match c {
+                '(' | ')' | '"' | '\'' => break,
+                c if c.is_whitespace() => break,
+                c => {
+                    self.advance();
+                    string.push(if c == '\\' {
+                        self.advance().map(translate_escape).unwrap_or('\\')
+                    } else {
+                        c
+                    });
+                },
+            }
+        }
+
+        Ok(Expr::ExpandableString(string.into()))
+    }
+
+    fn parse_string(&mut self) -> Result<Expr, ParseError> {
+        let delimiter = match self.advance() {
+            Some(c) if c == '"' || c == '\'' => c,
+            _ => panic!("invalid string delimiter"),
+        };
+        let mut string = String::new();
+
+        loop {
+            match self.advance() {
+                Some('\\') => string.push(self.advance()
+                    .map(translate_escape)
+                    .unwrap_or('\\')),
+                Some(c) if c == delimiter => break,
+                Some(c) => string.push(c),
+                None => return Err(self.error("ParseErrorKind::UnclosedString")),
+            }
+        }
+
+        Ok(Expr::ExpandableString(string.into()))
+    }
+
+    /// Consume and skip over any whitespace and comments.
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                Some(';') | Some('#') => {
+                    loop {
+                        match self.advance() {
+                            None | Some('\n') => break,
+                            _ => continue,
+                        }
+                    }
+                },
+                Some(c) if c.is_whitespace() => {
+                    self.advance();
+                },
+                _ => break,
+            }
+        }
+    }
+
+    fn peek(&mut self) -> Option<char> {
+        if self.next_char.is_none() {
+            self.next_char = match self.reader.read_char() {
+                Ok(Some(c)) => Some(c),
+                Ok(None) => None,
+                Err(_) => None,
+            };
+        }
+
+        self.next_char
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        let c = self.next_char.take().or_else(|| {
+            self.reader.read_char().unwrap()
+        });
+
+        self.pos.offset += 1;
+
+        if c == Some('\n') {
+            self.pos.line += 1;
+            self.pos.column = 1;
+        } else if c.is_some() {
+            self.pos.column += 1;
+        }
+
+        c
+    }
+
+    fn match_any(&mut self, chars: &[char]) -> bool {
+        for c in chars {
+            if self.peek() == Some(*c) {
+                self.advance();
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn error<M: Into<Cow<'static, str>>>(&self, message: M) -> ParseError {
         ParseError {
             message: message.into(),
-            pos: pos,
+            pos: self.pos,
         }
     }
 }
 
-/// Helper macro for creating parse errors.
-macro_rules! parse_error {
-    ($message:expr, $scanner:expr) => {
-        return Err(ParseError::new($message, $scanner.pos().clone()))
-    }
-}
-
-
-
-/// Parse the characters from the given scanner into an AST.
-pub fn parse(scanner: &mut Scanner) -> Result<Node, ParseError> {
-    /// Hey, a program is just a block!
-    parse_block(scanner)
-}
-
-fn parse_block(scanner: &mut Scanner) -> Result<Node, ParseError> {
-    // A block is just a series of 0 or more expressions.
-    Node::Nil
-}
-
-fn parse_expr(scanner: &mut Scanner) -> Result<Expression, ParseError> {
-    match scanner.next() {
-        Some('{') => {
-            let block = parse_block(scanner)?;
-
-            if scanner.next() == Some('}') {
-                parse_error!("expected }", scanner);
-            }
-
-            Ok(block)
-        },
-        Some('(') => parse_list(scanner),
-        Some(')') => parse_error!("extra trailing parenthesis", scanner),
-        Some('"') | Some('\'') => self.parse_string(),
-        Some(_) => parse_symbol(scanner),
-        None => {
-            parse_error!("unexpected eof, expected expression", scanner);
-        }
-    }
-}
-
-fn parse_list(scanner: &mut Scanner) -> Result<Expression, ParseError> {
-    assert!(scanner.next() == Some('('));
-    let mut items = Vec::new();
-
-    loop {
-        skip_whitespace(scanner);
-
-        match scanner.peek() {
-            Some(')') => {
-                scanner.next();
-                break;
-            },
-            Some(_) => items.push(parse_expr(scanner)?),
-            None => parse_error!("unclosed list", scanner),
-        }
-    }
-
-    Ok(Expression::List(items))
-}
-
-fn parse_symbol(scanner: &mut Scanner) -> Result<Expression, ParseError> {
-    let mut string = String::new();
-
-    // Read the first character of the symbol.
-    string.push(scanner.next().expect("expected symbol"));
-
-    // Read any remaining characters that are part of the symbol.
-    while let Some(c) = scanner.peek() {
-        match c {
-            '(' | ')' | '"' | '\'' => break,
-            c if c.is_whitespace() => break,
-            c => {
-                scanner.next();
-                string.push(if c == '\\' {
-                    scanner.next().map(translate_escape).unwrap_or('\\')
-                } else {
-                    c
-                });
-            },
-        }
-    }
-
-    Ok(Expression::Atom(string.into()))
-}
-
-fn parse_string(scanner: &mut Scanner) -> Result<Expression, ParseError> {
-    let delimiter = match self.next() {
-        Some(c) if c == '"' || c == '\'' => c,
-        _ => panic!("invalid string delimiter"),
-    };
-    let mut string = String::new();
-
-    loop {
-        match self.next() {
-            Some('\\') => string.push(self.next()
-                .map(translate_escape)
-                .unwrap_or('\\')),
-            Some(c) if c == delimiter => break,
-            Some(c) => string.push(c),
-            None => return parse_error!(self, ParseErrorKind::UnclosedString),
-        }
-    }
-
-    Ok(Expression::Atom(string.into()))
-}
-
-/// Consume and skip over any whitespace and comments.
-fn skip_whitespace(&mut self) {
-    loop {
-        match self.peek() {
-            Some(';') | Some('#') => {
-                loop {
-                    match self.next() {
-                        None | Some('\n') => break,
-                        _ => continue,
-                    }
-                }
-            },
-            Some(c) if c.is_whitespace() => {
-                self.next();
-            },
-            _ => break,
-        }
-    }
-}
 
 /// Get the value corresponding to a given escape character.
 fn translate_escape(c: char) -> char {
--- src/ripflow/reader.rs
+++ src/ripflow/reader.rs
@@ -1,133 +1,40 @@
 //! Provides types for reading files and strings incrementally.
-use std::fs::File;
 use std::io::{self, Read};
-use std::path::Path;
 use std::collections::VecDeque;
 use utf8parse;
 
 
-/// A reference to a location in source code. Useful for error messages.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct SourcePos {
-    /// The line number. Begins at 1.
-    pub line: u32,
-
-    /// The column position in the current line. Begins at 1.
-    pub column: u32,
-
-    /// Offset fron the beginning of the source.
-    pub offset: u32,
-}
-
-impl Default for SourcePos {
-    fn default() -> SourcePos {
-        SourcePos {
-            line: 1,
-            column: 1,
-            offset: 0,
-        }
-    }
-}
-
-
-/// Reads characters from a source incrementally.
-pub struct SourceReader {
-    name: String,
-    /// An optional stream to read additional bytes from.
+/// Reads characters from a stream incrementally.
+pub struct CharReader {
     reader: Option<Box<Read>>,
-    /// Internal byte buffer.
     buffer: VecDeque<u8>,
-    next_char: Option<char>,
-    pos: SourcePos,
     parser: utf8parse::Parser,
 }
 
-impl SourceReader {
+impl CharReader {
     const READ_SIZE: usize = 8192;
-    const DEFAULT_NAME: &'static str = "<unknown>";
 
     /// Create a new source reader from a sequence of bytes.
-    pub fn from_bytes<S, B>(name: S, bytes: B) -> Self
-        where S: Into<String>,
-              B: Into<Vec<u8>>,
-    {
+    pub fn from_bytes<B: Into<Vec<u8>>>(bytes: B) -> Self {
         let bytes: Vec<u8> = bytes.into();
         Self {
-            name: name.into(),
             buffer: bytes.into(),
             reader: None,
-            next_char: None,
-            pos: SourcePos::default(),
             parser: utf8parse::Parser::new(),
         }
     }
 
     /// Create a new source reader from a byte reader.
-    pub fn from_reader<S, R>(name: S, reader: R) -> Self
-        where S: Into<String>,
-              R: Read + 'static,
-    {
+    pub fn from_reader<R: Read + 'static>(reader: R) -> Self {
         Self {
-            name: name.into(),
             buffer: VecDeque::new(),
             reader: Some(Box::new(reader)),
-            next_char: None,
-            pos: SourcePos::default(),
             parser: utf8parse::Parser::new(),
         }
     }
 
-    /// Open a file for reading.
-    pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        let path = path.as_ref();
-        let name = path.to_string_lossy().into_owned();
-
-        Ok(Self::from_reader(name, File::open(path)?))
-    }
-
-    pub fn name(&self) -> &str {
-        &self.name
-    }
-
-    /// Report the current source position.
-    pub fn pos(&self) -> SourcePos {
-        self.pos
-    }
-
-    /// Get the next character of input.
-    pub fn next(&mut self) -> Option<char> {
-        let c = self.next_char.take().or_else(|| {
-            self.read_char().unwrap()
-        });
-
-        match c {
-            Some('\n') => {
-                self.pos.line += 1;
-                self.pos.column = 1;
-            },
-            Some(_) => {
-                self.pos.column += 1;
-            },
-            None => {},
-        }
-
-        c
-    }
-
-    /// Peek ahead one character.
-    pub fn peek(&mut self) -> Option<char> {
-        if self.next_char.is_none() {
-            self.next_char = match self.read_char() {
-                Ok(Some(c)) => Some(c),
-                Ok(None) => None,
-                Err(_) => None,
-            };
-        }
-
-        self.next_char.clone()
-    }
-
-    fn read_char(&mut self) -> io::Result<Option<char>> {
+    /// Read the next character.
+    pub fn read_char(&mut self) -> io::Result<Option<char>> {
         struct Receiver {
             last_char: Option<char>,
             invalid: bool,
@@ -184,23 +91,23 @@ impl SourceReader {
     }
 }
 
-impl From<String> for SourceReader {
-    fn from(string: String) -> SourceReader {
-        SourceReader::from_bytes(SourceReader::DEFAULT_NAME, string)
+impl From<String> for CharReader {
+    fn from(string: String) -> CharReader {
+        CharReader::from_bytes(string)
     }
 }
 
-impl<'a> From<&'a str> for SourceReader {
-    fn from(string: &str) -> SourceReader {
-        SourceReader::from_bytes(SourceReader::DEFAULT_NAME, string)
+impl<'a> From<&'a str> for CharReader {
+    fn from(string: &str) -> CharReader {
+        CharReader::from_bytes(string)
     }
 }
 
-impl Iterator for SourceReader {
+impl Iterator for CharReader {
     type Item = char;
 
     fn next(&mut self) -> Option<char> {
-        SourceReader::next(self)
+        self.read_char().ok().and_then(|c| c)
     }
 }
 
@@ -214,7 +121,7 @@ mod tests {
     #[test]
     fn test_read_from_string() {
         let s = "hello world";
-        let mut reader = SourceReader::from_bytes("<unknown>", s);
+        let mut reader = CharReader::from_bytes(s);
 
         for expected_char in s.chars() {
             let actual_char = reader.next().unwrap();
@@ -229,7 +136,7 @@ mod tests {
     fn test_read_from_reader() {
         let s = "hello world";
         let cursor = Cursor::new(s);
-        let mut reader = SourceReader::from_reader("<unknown>", cursor);
+        let mut reader = CharReader::from_reader(cursor);
 
         for expected_char in s.chars() {
             let actual_char = reader.next().unwrap();
