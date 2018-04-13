c3864edaddd657ecd198cc4d1023b0612105e765
Stephen M. Coakley <me@stephencoakley.com>
2018-04-13 14:41:06 -0500

    improve exception type

--- riptide/src/exceptions.rs
+++ riptide/src/exceptions.rs
@@ -2,6 +2,10 @@ use std::fmt;
 use std::io;
 use value::Value;
 
+/// An exception thrown at runtime.
+///
+/// An exception is a value (typically a string) that is _thrown_ used to indicate some sort of error detected during
+/// runtime of a program.
 #[derive(Clone, Debug)]
 pub struct Exception {
     message: Value,
@@ -9,6 +13,9 @@ pub struct Exception {
 }
 
 impl Exception {
+    /// Create a new exception with a message.
+    ///
+    /// Usually the message is a string, but technically it could be any value type.
     pub fn new<M: Into<Value>>(message: M) -> Self {
         Self {
             message: message.into(),
@@ -16,10 +23,29 @@ impl Exception {
         }
     }
 
+    /// Create a new exception with a message and another exception that caused this one.
+    ///
+    /// Causes can be chained together, almost like a linked list. This is useful for debugging, as it can provide a
+    /// poor man's kind of "trace" of errors to help find the root cause.
     pub fn with_cause<M: Into<Value>>(message: M, cause: Exception) -> Self {
         Self {
             message: message.into(),
             cause: Some(Box::new(cause)),
+        }
+    }
+
+    /// Get the exception message.
+    #[inline]
+    pub fn message(&self) -> &Value {
+        &self.message
+    }
+
+    /// Get the cause of the exception, if present.
+    #[inline]
+    pub fn cause(&self) -> Option<&Exception> {
+        match self.cause {
+            Some(ref cause) => Some(cause),
+            None => None,
         }
     }
 }
