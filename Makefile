80a31e63bd0c8a68f478250dd9f7d85afcd6a1cf
Stephen M. Coakley <me@stephencoakley.com>
2019-01-11 23:04:19 -0600

    Add script for publishing docs to GitHub pages

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-.PHONY: docs
+.PHONY: docs publish-docs
 
 docs: $(patsubst docs/%.adoc,target/docs/%.html,$(wildcard docs/*.adoc))
+
+publish-docs: docs
+	scripts/publish-docs.sh
 
 target/docs/%.html: docs/%.adoc
 	@mkdir -p target/docs
---
+++ scripts/publish-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+COMMIT_HASH=$(git rev-parse HEAD)
+COMMIT_MESSAGE="Deploy $COMMIT_HASH"
+ORIGIN=$(git remote get-url origin)
+
+rm -rf target/docs/.git
+git -C target/docs init .
+git -C target/docs add -A
+git -C target/docs commit -m "$COMMIT_MESSAGE"
+git -C target/docs push "$ORIGIN" master:gh-pages --force
+rm -rf target/docs/.git
