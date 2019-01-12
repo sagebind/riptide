39501307ac72eb5ceac083e596e7c0adfd877714
Stephen M. Coakley <me@stephencoakley.com>
2019-01-12 00:35:58 -0600

    Tweak publish script to get GitHub to rebuild docs

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -3,15 +3,15 @@ set -eu
 
 COMMIT_HASH=$(git rev-parse HEAD)
 COMMIT_MESSAGE="Deploy $COMMIT_HASH"
-ORIGIN="https://sagebind:$GITHUB_TOKEN@github.com/sagebind/riptide.git"
+ORIGIN="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 rm -rf target/docs/.git
 
 git -C target/docs init .
-git -C target/docs config user.name "Stephen M. Coakley"
-git -C target/docs config user.email "me@stephencoakley.com"
-git -C target/docs add -A
-git -C target/docs commit -m "$COMMIT_MESSAGE"
-git -C target/docs push "$ORIGIN" master:gh-pages --force
+git -C target/docs config user.name "${GITHUB_ACTOR}"
+git -C target/docs config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git -C target/docs add .
+git -C target/docs commit -m "${COMMIT_MESSAGE}"
+git -C target/docs push "${ORIGIN}" master:gh-pages --force
 
 rm -rf target/docs/.git
