13dd429554877c63fb15c16806a19f08a522a754
Stephen M. Coakley <me@stephencoakley.com>
2019-08-27 14:35:39 -0500

    Make serde dependency optional

--- syntax/src/ast.rs
+++ syntax/src/ast.rs
@@ -1,10 +1,10 @@
 //! Abstract syntax tree definitions for the language syntax.
 
-use serde::{Serialize, Deserialize};
 use std::fmt;
 
 /// A function block, containing a list of pipelines to execute.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Block {
     /// A list of named parameters.
     pub named_params: Option<Vec<String>>,
@@ -14,21 +14,23 @@ pub struct Block {
 }
 
 /// A pipeline of function calls.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Pipeline(pub Vec<Call>);
 
 /// A function call.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Call {
     /// A function call for a named function.
-    #[serde(rename = "NamedCall")]
+    #[cfg_attr(feature = "serde", serde(rename = "NamedCall"))]
     Named {
         function: VariablePath,
         args: Vec<Expr>,
     },
 
     /// A function call on a callable object.
-    #[serde(rename = "UnnamedCall")]
+    #[cfg_attr(feature = "serde", serde(rename = "UnnamedCall"))]
     Unnamed {
         function: Box<Expr>,
         args: Vec<Expr>
@@ -38,8 +40,12 @@ pub enum Call {
 /// Abstract representation of an expression.
 ///
 /// Contains a variant for each different expression type.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-#[serde(untagged)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(untagged),
+)]
 pub enum Expr {
     Block(Block),
     Pipeline(Pipeline),
@@ -52,7 +58,8 @@ pub enum Expr {
 }
 
 /// Value substitution.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Substitution {
     /// A format substitution with a variable and parameters, such as `${foo:.2}`.
     ///
@@ -70,7 +77,8 @@ pub enum Substitution {
     Variable(VariablePath),
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct VariablePath(pub Vec<String>);
 
 impl fmt::Display for VariablePath {
@@ -79,16 +87,19 @@ impl fmt::Display for VariablePath {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct TableLiteral(pub Vec<TableEntry>);
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct TableEntry {
     pub key: Expr,
     pub value: Expr
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct ListLiteral(pub Vec<Expr>);
 
 
@@ -96,11 +107,16 @@ pub struct ListLiteral(pub Vec<Expr>);
 ///
 /// An interpolated string is made up of a sequence of parts that, when stringified and concatenated in order, form the
 /// desired string value.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct InterpolatedString(pub Vec<InterpolatedStringPart>);
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-#[serde(untagged)]
+#[derive(Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(untagged),
+)]
 pub enum InterpolatedStringPart {
     String(String),
     Substitution(Substitution),
